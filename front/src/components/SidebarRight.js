30c9c45d32ffc1791fa90ba30e31ac02958e457e
unknown <reinaldoagf1@gmail.com>
2024-10-09 18:04:21 -0400

    fix: sidebarright data updated

--- front/src/components/SidebarRight.js
+++ front/src/components/SidebarRight.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import SidebarItem from './SidebarItem';
-import { Icons } from '../consts';
-import { Artists } from '../consts';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDoctors } from '../redux/features/doctorsSlice';
 
 export default function SidebarRight() {
+  const dispatch = useDispatch();
+  const doctors = useSelector((state) => state.doctors.doctors);
+  const status = useSelector((state) => state.doctors.status);
+  const error = useSelector((state) => state.doctors.error);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchDoctors());
+    }
+  }, [status, dispatch]);
+
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'failed') return <p>Error: {error}</p>;
     return (
       <div className="p-3 md:w-72 shrink-0 md:sticky md:top-16 shrink-0 h-full">
-        <h2 className="text-xl font-semibold">Doctores más solicitados</h2>
+        <h2 className="text-xl font-semibold">Doctores</h2>
         <ul className="mt-3 space-y-3">
-          {Artists.map(({ name, handler, image }) => (
+          {doctors.map(({ nombre, especialidad, image }, index) => (
             <li
               className="bg-zinc-200 rounded-md p-2 flex shadow-lg"
-              key={handler}
+              key={index}
             >
-              <img
-                src={image}
-                className="w-12 h-12 rounded-md"
-                alt={`top artist ${name}`}
-              />
               <div className="ml-3">
-                <h3 className="font-semibold">{name}</h3>
-                <p className="text-sm text-zinc-400">{handler}</p>
+                <h3 className="font-semibold">{nombre}</h3>
+                <p className="text-sm text-zinc-400">{especialidad}</p>
               </div>
             </li>
           ))}
